2d7565ee2c29dff3b62fa13e7638362a5ddb36cf
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-13 01:22:19 +0000

    refactor(`CollectionAdapter`): change generic variable names.

--- src/lib/interface/collection-adapter.interface.ts
+++ src/lib/interface/collection-adapter.interface.ts
@@ -4,10 +4,11 @@ import { CollectionShape } from "./collection.shape";
  * @description The adapter interface for collections.
  * @export
  * @interface CollectionAdapter
- * @template Element  The type of elements in the collection.
- * @template Type The type of the value in the collection, data of elements.
- * @extends {CollectionShape<Element, Type>}
+ * @template E  The type of elements in the collection.
+ * @template T The type of the value in the collection, data of elements.
+ * @template R The `boolean` type to determine async methods.
+ * @extends {CollectionShape<E, T>}
  */
-export interface CollectionAdapter<Element, Type> extends CollectionShape<Element, Type> {
+export interface CollectionAdapter<E, T, R extends boolean = false> extends CollectionShape<E, T, R> {
   version: string;
 }
