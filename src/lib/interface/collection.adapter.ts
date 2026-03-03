5926abe8a32c71693c78bb358ba4d0e99ed3e31e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-03 10:57:16 +0000

    refactor(`CollectionAdapter`): enhance interface with generic templates for improved type inference and reorder

--- src/lib/interface/collection.adapter.ts
+++ src/lib/interface/collection.adapter.ts
@@ -1,18 +1,23 @@
 // Interface.
+import { CollectionSettings } from "./collection.settings";
 import { CollectionShape } from "./collection.shape";
+// Type.
+import { InferAsync, InferCollectionType, InferElement } from "../../type";
 /**
  * @description The adapter interface for collections.
  * @export
  * @interface CollectionAdapter
- * @template E  The type of elements in the collection.
- * @template T The type of the value in the collection, data of elements.
- * @template R The `boolean` type to determine async methods.
- * @extends {CollectionShape<E, T>}
+ * @template {CollectionSettings<E, T, R>} C 
+ * @template [E=InferElement<C>] The type of elements in the collection.
+ * @template [T=InferCollectionType<C>] The type of the collection.
+ * @template {boolean} [R=InferAsync<C>] The async behavior flag.
+ * @extends {CollectionShape<C, E, T, R>}
  */
 export interface CollectionAdapter<
-  E,
-  T,
-  R extends boolean = false
-> extends CollectionShape<E, T, R> {
-  version: string;
+  C extends CollectionSettings<E, T, R>,
+  E = InferElement<C>,
+  T = InferCollectionType<C>,
+  R extends boolean = InferAsync<C>,
+> extends CollectionShape<C, E, T, R> {
+  readonly version: string;
 }
