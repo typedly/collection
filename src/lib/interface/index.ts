921664e200d9cfc3044bb7f5d52234dfd0b7471f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-03 10:54:58 +0000

    feat(`AdaptableCollectionShape`): add interface for collection with adapter support.

--- src/lib/interface/index.ts
+++ src/lib/interface/index.ts
@@ -1,3 +1,4 @@
+export type { AdaptableCollectionShape } from './adaptable-collection.shape';
 export type { CollectionAdapter } from './collection.adapter';
 export type { CollectionSettings } from './collection.settings';
 export type { CollectionShape } from './collection.shape';
