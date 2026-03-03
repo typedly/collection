4f21057f85567b068441f6fe00befd8c18c99a6d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-03 10:57:40 +0000

    refactor(`CollectionShape`): enhance interface with generics for improved type inference and structure

--- src/lib/interface/collection.shape.ts
+++ src/lib/interface/collection.shape.ts
@@ -1,34 +1,25 @@
 // Interface.
 import { AsyncReturn, DataShape } from '@typedly/data';
 import { CollectionSettings } from './collection.settings';
+// Type.
+import { InferAsync, InferCollectionType, InferElement } from '../../type';
 /**
- * @description Represents a collection of elements.
+ * @description The `CollectionShape` interface defines the structure and behavior of a collection data structure, which can be implemented by various types of collections such as sets, arrays, or maps.
+ * It extends the `DataShape` interface, allowing it to inherit common data-related properties and methods while also introducing collection-specific functionalities.
  * @export
- * @interface Collection
- * @template E The type of elements in the collection.
- * @template [T=any] The type of the value in the collection, data of elements.
- * @template {boolean} [R=false] The `boolean` type to determine async methods.
- * @extends {DataShape<T, R>}
+ * @interface CollectionShape
+ * @template {CollectionSettings<E, T, R>} [C=CollectionSettings<any, any, any>] The configuration type.
+ * @template [E=InferElement<C>] The element type inferred from the configuration.
+ * @template [T=InferCollectionType<C>] The collection type inferred from the configuration.
+ * @template {boolean} [R=InferAsync<C>] The async behavior flag inferred from the configuration.
+ * @extends {DataShape<T, C, R>}
  */
 export interface CollectionShape<
-  E,
-  T = any,
-  R extends boolean = false
-> extends DataShape<T, R> {
-  /**
-   * @description Indicates whether the collection operates in asynchronous mode.
-   * @readonly
-   * @type {R}
-   */
-  readonly async: R;
-
-  /**
-   * @description The configuration settings of the collection.
-   * @readonly
-   * @type {?CollectionSettings<E, T, R>}
-   */
-  readonly configuration?: CollectionSettings<E, T, R>;
-
+  C extends CollectionSettings<E, T, R>,
+  E = InferElement<C>,
+  T = InferCollectionType<C>,
+  R extends boolean = InferAsync<C>
+> extends DataShape<T, C, R> {
   /**
    * @description The number of items in the collection.
    * @returns {number}
@@ -37,38 +28,32 @@ export interface CollectionShape<
 
   /**
    * @description Adds elements to the collection.
-   * @param {...E[]} element Element of type `T` to add.
+   * @param {...E[]} element Element of type `E` to add.
    * @returns {AsyncReturn<R, this>} The collection instance `this`, or in `Promise`.
    */
   add(...element: E[]): AsyncReturn<R, this>;
 
   /**
    * @description Deletes elements from the collection.
-   * @param {...E[]} element Element of type `T` to delete.
-   * @returns {AsyncReturn<R, boolean>} `true` if the element was successfully deleted, otherwise `false`.
+   * @param {...E[]} element Element of type `E` to delete.
+   * @returns {AsyncReturn<R, boolean>} `true` if every element was successfully deleted, otherwise `false`.
    */
   delete(...element: E[]): AsyncReturn<R, boolean>;
 
   /**
    * @description Executes a provided function once for each collection element.
-   * @param {(value: E, value2: E, collection: CollectionShape<E, T, R>) => void} callbackfn Function to execute for each element.
-   * @param {AsyncReturn<R, this>} [thisArg] Value to use as `this` when executing `callbackfn`.
+   * @param {(element: E, element2: E, collection: CollectionShape<C, E, T, R>) => void} callbackfn Function to execute for each element.
+   * @param {?*} [thisArg] Value to use as `this` when executing `callbackfn`.
+   * @returns {AsyncReturn<R, this>} 
    */
-  forEach(callbackfn: (element: E, element2: E, collection: CollectionShape<E, T, R>) => void, thisArg?: any): AsyncReturn<R, this>;
+  forEach(callbackfn: (element: E, element2: E, collection: CollectionShape<C, E, T, R>) => void, thisArg?: any): AsyncReturn<R, this>;
 
   /**
    * @description Checks if every item exists in the collection.
-   * @param {...E[]} element Element of type `T` to check for existence.
-   * @returns {AsyncReturn<R, boolean>} `true` if the element exists, otherwise `false`.
+   * @param {...E[]} element Element of type `E` to check for existence.
+   * @returns {AsyncReturn<R, boolean>} `true` if every element exists, otherwise `false`.
    */
   has(...element: E[]): AsyncReturn<R, boolean>;
-
-  /**
-   * @description Sets the asynchronous mode of the collection.
-   * @param {R} async The boolean type to determine async methods.
-   * @returns {this} The collection instance `this`.
-   */
-  setAsync?(async: R): this;
 
   /**
    * @description Converts the collection to an array of elements.
@@ -77,9 +62,9 @@ export interface CollectionShape<
   toArray?(): AsyncReturn<R, E[]>;
 
   /**
-   * @description
-   * @param {R} async 
-   * @returns {CollectionShape<E, T, R>} 
+   * @description Updates the collection's configuration settings and returns a new collection instance with the updated settings.
+   * @param {NC} settings The new configuration settings for the collection.
+   * @returns {CollectionShape<NC, E, T, R>} 
    */
-  with?(async: R): CollectionShape<E, T, R>;
+  with?<NC extends C>(settings: NC): CollectionShape<NC, E, T, R>;
 }
