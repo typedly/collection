b221fbda99cc5c229d6d15dba459c66f4ee31dd1
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-16 19:18:14 +0000

    refactor(`CollectionAdapter`): update import path and refine type parameters for improved clarity and consistency

--- src/adapter/lib/collection.adapter.ts
+++ src/adapter/lib/collection.adapter.ts
@@ -1,18 +1,18 @@
 // Type.
-import { CollectionShape } from "../../lib/interface/collection.shape";
+import { CollectionShape } from "../../lib";
 /**
  * @description The adapter interface for collections.
  * @export
  * @interface CollectionAdapter
- * @template E type of elements in the collection.
  * @template {Iterable<E>} T The type of the collection.
+ * @template E type of elements in the collection.
  * @template {boolean} R The async behavior flag.
- * @extends {CollectionShape<E, T, R>}
+ * @extends {CollectionShape<T, E, R>}
  */
 export interface CollectionAdapter<
-  E,
   T extends Iterable<E>,
+  E,
   R extends boolean,
-> extends CollectionShape<E, T, R> {
+> extends CollectionShape<T, E, R> {
   readonly version: string;
 }
