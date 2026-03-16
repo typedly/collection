d0678458bff049cb5fb7dd51ca0d3b37128bdd37
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-16 19:18:25 +0000

    refactor(`ConfigurableCollectionAdapter`): update template parameters for improved clarity and consistency

--- src/adapter/lib/configurable-collection.adapter.ts
+++ src/adapter/lib/configurable-collection.adapter.ts
@@ -1,22 +1,24 @@
+// Interface.
 import { CollectionSettings } from "../../lib";
 import { ConfigurableCollectionShape } from "../../lib/interface";
+// Type.
 import { InferAsync, InferCollectionType, InferElement } from "../../type";
 /**
  * @description The `ConfigurableCollectionAdapter` interface defines a collection data structure that can be configured based on the provided settings.
  * It extends the `ConfigurableCollectionShape` interface, allowing it to inherit collection-specific functionalities while also supporting configurability through the settings pattern.
  * @export
  * @interface ConfigurableCollectionAdapter
- * @template {CollectionSettings<E, T, R>} C 
- * @template [E=InferElement<C>] 
- * @template {Iterable<E>} [T=InferCollectionType<C>] 
- * @template {boolean} [R=InferAsync<C>] 
- * @extends {ConfigurableCollectionShape<C, E, T, R>}
+ * @template {CollectionSettings<T, E, R>} C The collection settings type.
+ * @template {Iterable<E>} [T=InferCollectionType<C>] The type of the collection inferred from the collection settings or defaults to `unknown` if not specified.
+ * @template [E=InferElement<C>] The element type inferred from the collection settings or defaults to `unknown` if not specified.
+ * @template {boolean} [R=InferAsync<C>] The async behavior flag inferred from the collection settings or defaults to `false` if not specified.
+ * @extends {ConfigurableCollectionShape<C, T, E, R>}
  */
 export interface ConfigurableCollectionAdapter<
-  C extends CollectionSettings<E, T, R>,
-  E = InferElement<C>,
+  C extends CollectionSettings<T, E, R>,
   T extends Iterable<E> = InferCollectionType<C>,
+  E = InferElement<C>,
   R extends boolean = InferAsync<C>,
-> extends ConfigurableCollectionShape<C, E, T, R> {
+> extends ConfigurableCollectionShape<C, T, E, R> {
   readonly version: string;
 }
