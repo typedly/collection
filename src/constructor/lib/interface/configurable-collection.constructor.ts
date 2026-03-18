bab053c3509f2c645b81e48996d2c6e73287e660
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-18 10:32:13 +0000

    refactor(`ConfigurableCollectionConstructor`): update imports and refine type templates for clarity and accuracy

--- src/constructor/lib/interface/configurable-collection.constructor.ts
+++ src/constructor/lib/interface/configurable-collection.constructor.ts
@@ -1,26 +1,27 @@
 // Interface.
-import { CollectionSettings, CollectionShape } from "../../../lib";
+import { CollectionSettings, ConfigurableCollectionShape } from "../../../lib";
 // Type.
 import { ConstrainedConstructor } from "@typedly/constructor";
+import { InferAsync, InferCollectionType, InferElement } from "../../../inference/lib";
 /**
  * @description The constructor type for `ConfigurableCollection`.
  * @export
  * @interface ConfigurableCollectionConstructor
+ * @template {CollectionSettings<E, T, R>} [C=CollectionSettings<E, T, R>] The collection settings type.
  * @template E The type of the elements in the collection.
  * @template T The type of the value in the collection, data of elements.
  * @template {boolean} [R=false] The boolean type indicates the async methods.
  * @template {CollectionShape<E, T, R>} [S=CollectionShape<E, T, R>] The collection shape type.
- * @template {CollectionSettings<E, T, R>} [C=CollectionSettings<E, T, R>] The collection settings type.
  * @extends {ConstrainedConstructor<CollectionShape<E, T, R>, S, [C, ...E[]]>}
  */
 export interface ConfigurableCollectionConstructor<
-  E,
-  T,
-  R extends boolean = false,
-  S extends CollectionShape<E, T, R> = CollectionShape<E, T, R>,
-  C extends CollectionSettings<E, T, R> = CollectionSettings<E, T, R>
+  S extends ConfigurableCollectionShape<C, T, E, R>,
+  C extends CollectionSettings<T, E, R> = S extends ConfigurableCollectionShape<infer U, any, any, any> ? U : CollectionSettings<any, any, any>,
+  T extends Iterable<E> = InferCollectionType<C>,
+  E = InferElement<C>,
+  R extends boolean = InferAsync<C>,
 > extends ConstrainedConstructor<
-  CollectionShape<E, T, R>,
+  ConfigurableCollectionShape<C, T, E, R>,
   S,
   [C, ...E[]]
 >{}
