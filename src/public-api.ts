0ec361687e8e9f0c61a4efbf1038cd6f9ba93f7e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-18 10:16:24 +0000

    chore(`public-api.ts`): reorganize type exports for improved clarity and structure

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,13 +2,26 @@
  * Public API Surface of collection
  */
 export type {
+  CollectionAdapter,
+  ConfigurableCollectionAdapter,
+} from './adapter';
+export type {
+  AdaptableCollectionConstructor,
   CollectionAdapterConstructor,
   CollectionConstructor,
+  ConfigurableCollectionConstructor,
   ConfigurableCollectionAdapterConstructor,
 } from './constructor';
 export type {
+  InferAsync,
+  InferCollectionType,
+  InferElementFromSettings,
+  InferElement,
+} from './inference';
+export type {
   // Interface.
-  CollectionAdapter,
+  AdaptableCollectionShape,
   CollectionSettings,
   CollectionShape,
+  ConfigurableCollectionShape,
 } from './lib';
