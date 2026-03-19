8331ee2aa79d62bec76ce8b9afdb0c5236c8d43e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-19 15:20:32 +0000

    refactor(`CollectionAdapter`): update type templates for elements and async behavior

--- src/adapter/lib/collection.adapter.ts
+++ src/adapter/lib/collection.adapter.ts
@@ -1,18 +1,33 @@
 // Type.
+import { IterableElement } from "@typedly/data";
+// Interface.
 import { CollectionShape } from "../../lib";
 /**
  * @description The adapter interface for collections.
  * @export
  * @interface CollectionAdapter
  * @template {Iterable<E>} T The type of the collection.
- * @template E type of elements in the collection.
- * @template {boolean} R The async behavior flag.
- * @extends {CollectionShape<T, E, R>}
+ * @template [E=IterableElement<T>] The type of elements in the collection.
+ * @template {boolean} [R=false] The async behavior flag.
+ * @extends {CollectionShape<T, E, R>} The collection shape interface.
+ * @since 
+ * @version 
+ * @author Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
+ * @see {@link CollectionShape}
+ * @see {@link IterableElement}
+ * @example
+ * ```ts
+ * import { CollectionAdapter } from "@typedly/collection";
+ *
+ * class MyCollectionAdapter implements CollectionAdapter<string[], string, false> {
+ *   readonly version = "1.0.0";
+ *  // Implement the methods defined in CollectionShape... 
+ * }
  */
 export interface CollectionAdapter<
   T extends Iterable<E>,
-  E,
-  R extends boolean,
+  E = IterableElement<T>,
+  R extends boolean = false,
 > extends CollectionShape<T, E, R> {
   readonly version: string;
 }
