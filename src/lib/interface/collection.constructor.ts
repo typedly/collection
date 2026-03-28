340d5abd51c012c4a66582a8f85eed7d3b696c80
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-28 12:57:12 +0000

    refactor(`CollectionConstructor`): simplify type inference for collection elements and async behavior

--- src/lib/interface/collection.constructor.ts
+++ src/lib/interface/collection.constructor.ts
@@ -1,22 +1,24 @@
 // Interface.
+import { InferAsync } from "@typedly/data";
 import { CollectionShape } from "./collection.shape";
 // Type.
 import { ConstrainedConstructor } from "@typedly/constructor";
+import { InferCollectionType, InferElement } from "../../inference";
 /**
  * @description The constructor type for `CollectionShape`.
  * @export
  * @interface CollectionConstructor
  * @template {CollectionShape<T, E, R>} S The collection shape type.
- * @template {Iterable<E>} [T=S extends CollectionShape<infer U, any, any> ? U : unknown] The type of the elements in the collection, inferred from the collection shape or defaults to `unknown` if not specified.
- * @template [E=S extends CollectionShape<any, infer U, any> ? U : unknown] The element type inferred from the collection shape or defaults to `unknown` if not specified.
- * @template {boolean} [R=S extends CollectionShape<any, any, infer U> ? U : false] The async behavior flag inferred from the collection shape or defaults to `false` if not specified.
+ * @template {Iterable<E>} [T=InferCollectionType<S>] The type of the elements in the collection, inferred from the collection shape or defaults to `unknown` if not specified.
+ * @template [E=InferElement<S>] The element type inferred from the collection shape or defaults to `unknown` if not specified.
+ * @template {boolean} [R=InferAsync<S>] The async behavior flag inferred from the collection shape or defaults to `false` if not specified.
  * @extends {ConstrainedConstructor<CollectionShape<T, E, R>, S, E[]>}
  */
 export interface CollectionConstructor<
   S extends CollectionShape<T, E, R>,
-  T extends Iterable<E> = S extends CollectionShape<infer U, any, any> ? U : unknown,
-  E = S extends CollectionShape<any, infer U, any> ? U : unknown,
-  R extends boolean = S extends CollectionShape<any, any, infer U> ? U : false,
+  T extends Iterable<E> = InferCollectionType<S>,
+  E = InferElement<S>,
+  R extends boolean = InferAsync<S>,
 > extends ConstrainedConstructor<
   CollectionShape<T, E, R>,
   S,
