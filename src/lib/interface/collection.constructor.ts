648b02c31411e52b0ff5c170aba057f6bdaee40f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 16:44:28 +0000

    refactor(CollectionConstructor): update generics and improve type inference for collection shapes

--- src/lib/interface/collection.constructor.ts
+++ src/lib/interface/collection.constructor.ts
@@ -1,26 +1,29 @@
-// Interface.
-import { InferAsync } from "@typedly/data";
+// Interface & Type.
 import { CollectionShape } from "./collection.shape";
-// Type.
 import { ConstrainedConstructor } from "@typedly/constructor";
-import { InferCollectionType, InferElement } from "../../inference";
+import { InferAsync, IterableElement } from "@typedly/data";
+import { InferCollectionType } from "../../inference";
 /**
  * @description The constructor type for `CollectionShape`.
  * @export
  * @interface CollectionConstructor
- * @template {CollectionShape<T, E, R>} S The collection shape type.
- * @template {Iterable<E>} [T=InferCollectionType<S>] The type of the elements in the collection, inferred from the collection shape or defaults to `unknown` if not specified.
- * @template [E=InferElement<S>] The element type inferred from the collection shape or defaults to `unknown` if not specified.
- * @template {boolean} [R=InferAsync<S>] The async behavior flag inferred from the collection shape or defaults to `false` if not specified.
- * @extends {ConstrainedConstructor<CollectionShape<T, E, R>, S, E[]>}
+ * @template {CollectionShape<T, E, S>} I The collection shape type.
+ * @template {Iterable<E>} [T=InferCollectionType<I>] The type of the elements in the collection, inferred from the collection shape or defaults to `unknown` if not specified.
+ * @template [E=IterableElement<T>] The element type inferred from the collection type `T`.
+ * @template {boolean} [S=InferAsync<I>] The async behavior flag inferred from the collection shape or defaults to `false` if not specified.
+ * @extends {ConstrainedConstructor<CollectionShape<T, E, S>, I, E[]>}
+ * @see {@link CollectionShape}
+ * @see {@link InferCollectionType}
+ * @see {@link IterableElement}
+ * @see {@link InferAsync}
  */
 export interface CollectionConstructor<
-  S extends CollectionShape<T, E, R>,
-  T extends Iterable<E> = InferCollectionType<S>,
-  E = InferElement<S>,
-  R extends boolean = InferAsync<S>,
+  I extends CollectionShape<T, E, S>,
+  T extends Iterable<E> = InferCollectionType<I>,
+  E = IterableElement<T>,
+  S extends boolean = InferAsync<I>,
 > extends ConstrainedConstructor<
-  CollectionShape<T, E, R>,
-  S,
+  CollectionShape<T, E, S>,
+  I,
   E[]
 > {}
