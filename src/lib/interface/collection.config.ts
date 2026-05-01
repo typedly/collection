718c054ab5021be4ba66786aea84922be9425efa
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 16:44:09 +0000

    refactor(`CollectionConfig`): set default value to `E` from `T` and `S` (async)

--- src/lib/interface/collection.config.ts
+++ src/lib/interface/collection.config.ts
@@ -1,19 +1,22 @@
 // Abstract.
 import { CollectionSettings } from "./collection.settings";
 // Type.
-import type { DataConfig } from '@typedly/data';
+import type { DataConfig, InferAsync, IterableElement } from '@typedly/data';
 /**
  * @description The configuration type for collections, which extends the generic `DataConfig` type from `@typedly/data` with specific parameters related to collection settings, collection type, element type, and async behavior.
  * This type is used to define the configuration structure for collection data structures, allowing for flexible and customizable configurations based on the specific needs of the collection implementation.
  * @export
- * @template {CollectionSettings<T, E, R>} C The collection settings type that extends `CollectionSettings` with collection type `T`, element type `E`, and async behavior `R`.
+ * @template {CollectionSettings<T, E, S>} C The collection settings type that extends `CollectionSettings` with collection type `T`, element type `E`, and async behavior `S`.
  * @template {Iterable<E>} T The collection type.
  * @template E The element type.
- * @template {boolean} [R=false] The async behavior flag.
+ * @template {boolean} [S=InferAsync<C>] The async behavior flag, inferred from the collection settings type `C`.
+ * @see {@link CollectionSettings}
+ * @see {@link IterableElement}
+ * @see {@link InferAsync}
  */
 export type CollectionConfig<
-  C extends CollectionSettings<T, E, R>,
+  C extends CollectionSettings<T, E, S>,
   T extends Iterable<E>,
-  E,
-  R extends boolean = false,
-> = DataConfig<C, R>;
+  E = IterableElement<T>,
+  S extends boolean = InferAsync<C>,
+> = DataConfig<C, S>;
