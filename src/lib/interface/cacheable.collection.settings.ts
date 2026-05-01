650cc7bc6a1a2b5de9b4665390d5b3cd4c92a4f7
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-05-01 16:42:01 +0000

    feat: add `CacheableCollectionSettings` interface for cacheable collections

---
+++ src/lib/interface/cacheable.collection.settings.ts
@@ -0,0 +1,17 @@
+// Interface & Type
+import type { CacheableSettings, IterableElement } from '@typedly/data';
+import type { CollectionSettings } from './collection.settings';
+/**
+ * @description The `CacheableCollectionSettings` interface defines the settings for a cacheable collection, extending both `CollectionSettings` and `CacheableSettings`.
+ * It includes generic parameters for the collection type `T`, the element type `E`, and an optional read-only flag `R`.
+ * This interface allows for configuring cacheable collections with specific settings for data operations while maintaining the structure of a collection.
+ * @export
+ * @interface CacheableCollectionSettings
+ * @template {Iterable<E>} T The type of collection, which must be an iterable of elements of type `E`.
+ * @template [E=IterableElement<T>] The type of elements in the collection, inferred from the collection type `T`.
+ * @template {boolean} [R=false] The read-only flag, indicating whether the collection is read-only.
+ * @extends {CollectionSettings<T, E, R>} The collection settings that define the structure and behavior of the collection.
+ * @extends {CacheableSettings<T>} The cacheable settings that define the caching behavior for the collection.
+ */
+export interface CacheableCollectionSettings<T extends Iterable<E>, E = IterableElement<T>, R extends boolean = false>
+  extends CollectionSettings<T, E, R>, CacheableSettings<T> {}
